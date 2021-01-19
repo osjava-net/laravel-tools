936b5ad75fa8a136c21e8d3e07e41652859d2380
samboy2002 <samboy@osjava.net>
2021-01-19 15:26:45 +0800

    Modify the method name from generateString to __generateString

--- src/Support/RandomString.php
+++ src/Support/RandomString.php
@@ -2,18 +2,18 @@
 trait RandomString
 {
     function random_alpha($length) {
-        return $this->generate('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', $length);
+        return $this->__generateString('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', $length);
     }
 
     function random_number($length) {
-        return $this->generate('0123456789', $length);
+        return $this->__generateString('0123456789', $length);
     }
 
     function random_alpha_number($length) {
-        return $this->generate('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', $length);
+        return $this->__generateString('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', $length);
     }
 
-    private function generate($source, $length) {
+    private function __generateString($source, $length) {
         $max = strlen($source);
         $dest = '';
         for ($index = 0; $index < $length; $index++) {
